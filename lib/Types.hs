4a421583613cb14b0f3742548c7c305d993b414f
abhaasgoyal <u7145384@anu.edu.au>
2021-02-01 05:19:45 +0530

    Readded show instance to LamExpr

--- lib/Types.hs
+++ lib/Types.hs
@@ -7,9 +7,9 @@ Author      : Abhaas Goyal
 module Types where
 
 -- Lamda Calculus Types
-data LamMacroExpr = LamDef [ (String,LamExpr) ] LamExpr deriving (Eq, Read)
+data LamMacroExpr = LamDef [ (String,LamExpr) ] LamExpr deriving (Eq, Show, Read)
 data LamExpr = LamMacro String | LamApp LamExpr LamExpr  |
-               LamAbs Int LamExpr  | LamVar Int deriving (Eq,Read)
+               LamAbs Int LamExpr  | LamVar Int deriving (Eq, Show, Read)
 
 -- Word Search Types
 type WordSearchGrid = [[ Char ]]
